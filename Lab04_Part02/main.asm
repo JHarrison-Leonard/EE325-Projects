84684ecfeb8c8ecaf81acd4549495665c042837d
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-01-30 17:41:51 -0600

    Began the main loop, added the inputStr

--- Lab04_Part02/main.asm
+++ Lab04_Part02/main.asm
@@ -9,7 +9,7 @@
 ; Date:        Febuary 4th, 2020
 ;-------------------------------------------------------------------------------
             .cdecls C,LIST,"msp430.h"       ; Include device header file
-            
+
 ;-------------------------------------------------------------------------------
             .def    RESET                   ; Export program entry-point to
                                             ; make it known to linker.
@@ -20,6 +20,8 @@
             .retainrefs                     ; And retain any sections that have
                                             ; references to current section.
 
+inputStr:	.cstring "6*9"
+
 ;-------------------------------------------------------------------------------
 RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
 StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
@@ -28,15 +30,17 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
+main:		mov.w	#inputStr, R4			;
+			call	#getNum					;
+			jz		
 
-                                            
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
 ;-------------------------------------------------------------------------------
             .global __STACK_END
             .sect   .stack
-            
+
 ;-------------------------------------------------------------------------------
 ; Interrupt Vectors
 ;-------------------------------------------------------------------------------
