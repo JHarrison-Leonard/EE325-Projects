7be079a4cf52a1c8c6c17204f0a7b7e9d162c97c
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-02-03 13:45:53 -0600

    Finished code logic and code comments, needs veriftication

--- Lab04_Bonus/main.asm
+++ Lab04_Bonus/main.asm
@@ -13,12 +13,19 @@
 ;-------------------------------------------------------------------------------
             .def    RESET                   ; Export program entry-point to
                                             ; make it known to linker.
+
+			.data
+painBool:	.byte	'N'						;
+
 ;-------------------------------------------------------------------------------
             .text                           ; Assemble into program memory.
             .retain                         ; Override ELF conditional linking
                                             ; and retain current section.
             .retainrefs                     ; And retain any sections that have
                                             ; references to current section.
+
+inputStr:	.string	"20200202"				;
+inputStrE:									;
 
 ;-------------------------------------------------------------------------------
 RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
@@ -28,8 +35,18 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
-
-                                            
+main:		mov.w	#inputStr, R4			; Pointer to first char
+			mov.w	#inputStrE-1, R5		; Pointer to last char
+			mov.w	#inputStrE-inputStr, R6	; Size of inputStr
+mLoop:		rra.w	R6						; Half the size
+			cmp.b	@R4+, 0(R5)				; Compare characters
+			jnz		#paliNo					; Jump out if not palindromic
+			dec.b	R5						; Shift pointers towards center
+			dec.w	R6						; Decrement count
+			jnz		#mLoop					; Loop through full string
+			mov.b	#'Y', &paliBool			; If palindromic, set paliBool to Y
+paliNo:		
+done:		jmp		#done					;
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
