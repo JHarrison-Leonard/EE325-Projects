c1ac36576c6bafec70af52f02aeeb01da24cc506
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-02-03 12:42:59 -0600

    Finished isNum sub-process logic, started on getNum

--- Lab04_Part02/main.asm
+++ Lab04_Part02/main.asm
@@ -32,21 +32,43 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 main:		bis.b	#0xFF, &P1DIR			;
 			mov.w	#inputStr, R4			;
+			mov.w	#0xFF, R7				;
 			call	#getNum					;
-			jz		strFail					;
+			jnz		strFail					;
 			mov.b	#'*', R5				;
-			cmp.b	@R4+, R6				;
+			cmp.b	@R4+, R5				;
 			jne		strFail					;
 			mov.w	R6, R5					;
 			call	#getNum					;
 			jz		strFail					;
 			cmp.w	R5, R6					;
 			jge		multiply				;
-			
+			mov.w	R5, R7					;
+			mov.w	R6, R5					;
+			mov.w	R7, R6					;
+multiply:	clr.w	R7						;
+mLoop:		add.w	R6, R7					;
+			dec.w	R5						;
+			jnz		#mLoop					;
+strFail:	
+output:		bis.b	R7,	&P2OUT				;
+done:		jmp		#done					;
 
 getNum:		
+			call	#isNum					;
+			jez		#gnFail					;
+			
 
-isNum:		
+isNum:		mov.b	@R4+, R8				;
+			cmp.b	#'0', R8				;
+			jl		#inF					;
+			cmp.b	#'9'+1, R8				;
+			jl		#inT					;
+			clrz							;
+			ret								;
+inT:		setz							;
+inF:		ret								;
+			
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
