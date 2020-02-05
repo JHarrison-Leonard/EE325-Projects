cff7a7ec58201e718ae1509901850a77d0a99693
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-02-05 13:38:42 -0600

    Fixed sign logic (by using MPYS), fully commented, needs verifitcation

--- Lab05-Part01/HW_mult.asm
+++ Lab05-Part01/HW_mult.asm
@@ -1,9 +1,13 @@
 ;-------------------------------------------------------------------------------
 ; File:        HW_mult.asm
-; Function:    [Does...
-; Description: [Does that by...
-; Input:       [Takes inputs...
-; Output:      [Gives outputs...
+; Function:    Obtains the sum of squares of the integers in an array
+; Description: Function performed by iterating through the array and using the
+;              hardware multiplier to perform the square and adding the results
+;              to an output register
+; Input:       Given by the stack in ascending order (e.g. &arg1 < &arg2)
+;              arg1: Pointer to integer array
+;              arg2: Size of array
+; Output:      R13: Sum of the squares as an unsigned integer
 ; Author:      Justin H. Leonard
 ; Lab section: 04
 ; Date:        February 11th, 2020
@@ -16,20 +20,20 @@
 ;-------------------------------------------------------------------------------
 			.text
 
-HW_mult:	push	R4						;
+HW_mult:	push	R4						; Place register contents on stack
 			push	R5
 			mov.w	6(SP), R4				; Obtain argument 1
 			mov.w	8(SP), R5				; Obtain argument 2
 			clr		R13						; Clear output
-loop:		dec.w	R5
-			jge		exit
-			mov.w	@R4, &MPY
-			mov.w	@R4+, &OP2
-			add.w	&RESLO, R13
-			jmp		loop
-exit:		pop R5							;
+loop:		dec.w	R5						;{ Iterating through array
+			jge		exit					;  Exiting loop when array fully iterated
+			mov.w	@R4, &MPYS				;  Set operand 1 of signed multiply to element
+			mov.w	@R4+, &OP2				;  Set operand 2 to element
+			add.w	&RESLO, R13				;  Accumulate result in R13
+			jmp		loop					;} Continue looping through loop
+exit:		pop R5							; Retrieve register contents from stack
 			pop R4
-			ret								;
+			ret								; Return
 
 
 ;-------------------------------------------------------------------------------
