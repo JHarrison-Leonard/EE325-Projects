03992445a6b7cbd25bf42052690203daae0490d8
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-02-17 12:23:49 -0600

    Added logic for main loop

--- Lab06_Part01/main.asm
+++ Lab06_Part01/main.asm
@@ -31,18 +31,36 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 main:		bic.b	#BIT1, &P1DIR			; Set P1.1 to input for SW2
 			bis.b	#BIT1&BIT2, &P2DIR		; Set P2.1 and P2.2 to output for LED1 and LED2
 			mov.w	#WDT_ADLY_1000, &WDTCTL	; Set watchdog timer to 1s timer mode
+
 			nop
 			bis.w	#GIE, SR				; Enable global interrupts
 			nop
 			bis.b	#BIT1, &P1IE			; Enable P1.1 interrupt for SW2
-			bis.b	#BIT1, &P1IES			; P1.1 interrupt from high to low
-			bic.b	#BIT1, &P1IFG			; Clear interrupt flag for P1.1
+			bis.b	#BIT1, &P1IES			; SW2 interrupt from high to low
+			bic.b	#BIT1, &P1IFG			; Clear interrupt flag for SW2
 			bis.b	#WDTIE, &IE1			; Enable watchdog timer interrupts
 			bic.b	#WDTIFG, &IFG1			; Clear interrupt flag for watchdog timer
 
+			clr		R4						; Clear debounce press counter
+			clr		R5						; Clear debounce release counter
 mLoop:		call	msDelay					; Delay for 1 ms
-			; logic
-			jmp		mLoop					; Loop forever
+			cmp.w	#0, R4					; Test if press counter is not zero
+			jz		pSkip					; Skip press debounce logic otherwise
+			dec.w	R4						; Decrement pressed counter
+			cmp.w	#0, R4					; Test if press counter is zero
+			jnz		mLoop					; Relooping otherwise
+			mov.w	#20, R5					; Set release counter to 20
+			jmp		mLoop					; Relooping
+pSkip:		cmp.w	#0, R5					; Test if release counter is not zero
+			jz		mLoop					; Relooping otherwise
+			bit.b	#BIT1, &P1IN			; Test if SW2 is released
+			jnz		mLoop					; Relooping otherwise
+			dec.w	R5						; Decrement release counter
+			cmp.w	#0, R5					; Test if release counter is zero
+			jnz		mLoop					; Relooping otherwise
+			bic.b	#BIT1, &P1IFG			; Clear interrupt flags for SW2
+			bis.b	#BIT1, &P1IE			; Enable interrupts for SW2
+			jmp		mLoop					; Relooping
 
 
 msDelay:	ret
@@ -52,10 +70,10 @@ msDelay:	ret
 ; P1.X interrupt service routine
 ;-------------------------------------------------------------------------------
 P1_ISR:
-			bic.b	#BIT1, &P1IE			; Disable interrupts for P1.1
+			bic.b	#BIT1, &P1IE			; Disable interrupts for SW2
 			bic.b	#BIT1, &P1IFG			; Clear interrupt flag
 			xor.b	#BIT1, &P2OUT			; Toggle LED2
-			; Debounce logic
+			mov.w	#20, R4					; Set pressed counter to 20
 			reti
 
 
