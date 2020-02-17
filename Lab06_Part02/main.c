b14234a3361342b7afebd7fccd2676c56f82f345
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-02-17 14:20:32 -0600

    Added main loop logic

--- Lab06_Part02/main.c
+++ Lab06_Part02/main.c
@@ -17,8 +17,28 @@ int main(void)
 	// Stop watchdog timer
 	WDTCTL = WDTPW | WDTHOLD;
 	
-	// Main code
+	// Setup clock
+	FLL_CTL0 |= XCAP18PF	// Set capacitance
+	SCFI0 |= FN_2			// Set DCO range
+	SCFQCTL = 64			// (64 + 1) * 32768 = 2097152 MHz
+	
+	// Setup parallel ports
+	P1DIR &= ~(BIT0 | BIT1)	// Set P1.0 and P1.1 to input for SW1 and SW2
+	P2DIR |= BIT1			// Set P2.1 to output for LED2
+	
+	// Main loop
+	for(;;)
+	{
+		_delay_cycles(1048576);	// Delay for 1048576 cycles
+		P2OUT ^= BIT1			// Toggle LED2
+	}
 	
 	// Default return
 	return 0;
+}
+
+#pragma vector = PORT1_VECTOR
+__interrupt void Port1_ISR()
+{
+	
 }
