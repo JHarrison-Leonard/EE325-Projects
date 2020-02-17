589c0b1bbd3212a2fa8ddc5ac1735cbe50b4c539
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-02-17 16:32:34 -0600

    Added interrupt logic

--- Lab06_Part02/main.c
+++ Lab06_Part02/main.c
@@ -11,6 +11,9 @@
 #include <stdio.h>
 #include "main.h"
 
+#define SW1 !(P1IN & BIT0)
+#define SW2 !(P1IN & BIT1)
+
 
 int main(void)
 {
@@ -18,19 +21,25 @@ int main(void)
 	WDTCTL = WDTPW | WDTHOLD;
 	
 	// Setup clock
-	FLL_CTL0 |= XCAP18PF	// Set capacitance
-	SCFI0 |= FN_2			// Set DCO range
-	SCFQCTL = 64			// (64 + 1) * 32768 = 2097152 MHz
+	FLL_CTL0 |= DCOPLUS | XCAP18PF;	// Set capacitance
+	SCFI0 |= FN_2;					// Set DCO range
+	SCFQCTL = 63;					// (64 + 1) * 32768 = 2097152 MHz
 	
 	// Setup parallel ports
-	P1DIR &= ~(BIT0 | BIT1)	// Set P1.0 and P1.1 to input for SW1 and SW2
-	P2DIR |= BIT1			// Set P2.1 to output for LED2
+	P1DIR &= ~(BIT0 | BIT1);// Set P1.0 and P1.1 to input for SW1 and SW2
+	P2DIR |= BIT1;			// Set P2.1 to output for LED2
+	
+	// Setup interrupts
+	_EINT();
+	P1IE |= BIT0 | BIT1;	// Enable interrupts for P1.0 and P1.1 for SW1 and SW2
+	P1IES |= BIT0 | BIT1;	// Set interrupts for high low edge
+	P1IFG &= ~(BIT0 | BIT1);// Clear interrupt bit for SW1 and SW2
 	
 	// Main loop
 	for(;;)
 	{
 		_delay_cycles(1048576);	// Delay for 1048576 cycles
-		P2OUT ^= BIT1			// Toggle LED2
+		P2OUT ^= BIT1;			// Toggle LED2
 	}
 	
 	// Default return
@@ -40,5 +49,14 @@ int main(void)
 #pragma vector = PORT1_VECTOR
 __interrupt void Port1_ISR()
 {
-	
+	if(SW1)
+	{
+		SCFI0 &= ~FLLD_4;
+		SCFI0 |= FLLD_2;
+	}
+	else if(SW2)
+	{
+		SCFI0 &= ~FLLD_4;
+		SCFI0 |= FLLD_2;
+	}
 }
