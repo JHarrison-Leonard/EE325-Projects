262be4f87398641f73dd71d7c7cfb605434c7159
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-02-18 14:39:40 -0600

    Created project, implemented half of the logic from part 1

---
+++ Lab06_Bonus/main.c
@@ -0,0 +1,63 @@
+/*---------------------------------------------------------------------------
+ * File:        main.c
+ * Description: [Does...
+ * Input:       [Takes inputs...
+ * Output:      [Gives output...
+ * Author:      Justin H. Leonard
+ * Lab Section: 04
+ * Date:        February 20th, 2020
+ *---------------------------------------------------------------------------*/
+#include <msp430.h>
+
+#define SW2 !(BIT1 | P1IN)
+unsigned char SW2_debounce_counter_press;
+unsigned char SW2_debounce_counter_release;
+
+
+int main(void)
+{
+	// Stop watchdog timer
+	WDTCTL = WDTPW | WDTHOLD;
+	
+	// Setup peripherals
+	P1DIR &= ~BIT1;			// Set P1.1 to input for SW2
+	P2DIR |= BIT1 | BIT2;	// Set P2.1 and P2.2 to output for LED2 and LED1
+	WDTCTL = WDT_ADLY_1000;	// Set watchdog timer to 1s mode
+	
+	// Setup interrupts
+	_EINT();				// Enable global interrupts
+	P1IE |= BIT1;			// Enable P1.1 interrupt for SW2
+	P1IES |= BIT1;			// SW2 interrupt from high to low
+	P1IFG &= ~BIT1;			// Clear interrupt flag for SW2
+	IE1 |= WDTIE;			// Enable watchdog timer interrupts
+	IFG1 &= WDTIFG;			// Clear interrupt flag for watchdog timer
+	
+	SW2_debounce_counter_press = 0;
+	SW2_debounce_counter_release = 0;
+	for(;;)
+	{
+		msDelay();
+		if(SW2_debounce_counter_press)
+		{
+			SW2_debounce_counter_press--;
+			if(!SW2_debounce_counter_press)
+				SW2_debounce_counter_release = 20;
+		}
+		else if(SW2_debounce_counter_release && SW2)
+		{
+			SW2_debounce_counter_release--;
+			if(!SW2_debounce_counter_release)
+			{
+				P1IFG &= ~BIT1;
+				P1IE |= BIT1;
+			}
+		}
+	}
+	
+	// Default return, assumes execution success
+	return 0;
+}
+
+void msDelay()
+{
+}
