259ceda4f82c95ff227a737759c70966c9cad210
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-02-18 18:06:29 -0600

    Implemented bonus code, confirmed functional

--- Lab06_Bonus/main.c
+++ Lab06_Bonus/main.c
@@ -11,6 +11,9 @@
 #define F_CPU 1000000UL // 1 MHz
 #include <delay.h>
 
+#define SW1 !(BIT0 & P1IN)
+unsigned char SW1_debounce_counter_press;
+unsigned char SW1_debounce_counter_release;
 #define SW2 !(BIT1 & P1IN)
 unsigned char SW2_debounce_counter_press;
 unsigned char SW2_debounce_counter_release;
@@ -22,23 +25,40 @@ int main(void)
 	WDTCTL = WDTPW | WDTHOLD;
 	
 	// Setup peripherals
-	P1DIR &= ~BIT1;			// Set P1.1 to input for SW2
+	P1DIR &= ~(BIT0 | BIT1);// Set P1.0 and P1.1 to input for SW1 and SW2
 	P2DIR |= BIT1 | BIT2;	// Set P2.1 and P2.2 to output for LED2 and LED1
 	WDTCTL = WDT_ADLY_1000;	// Set watchdog timer to 1s mode
 	
 	// Setup interrupts
 	_EINT();				// Enable global interrupts
-	P1IE |= BIT1;			// Enable P1.1 interrupt for SW2
-	P1IES |= BIT1;			// SW2 interrupt from high to low
-	P1IFG &= ~BIT1;			// Clear interrupt flag for SW2
+	P1IE |= BIT0 | BIT1;	// Enable P1.0 and P1.1 interrupt for SW1 and SW2
+	P1IES |= BIT0 | BIT1;	// SW1 and SW2 interrupt from high to low
+	P1IFG &= ~(BIT0 | BIT1);// Clear interrupt flag for SW1 and SW2
 	IE1 |= WDTIE;			// Enable watchdog timer interrupts
 	IFG1 &= WDTIFG;			// Clear interrupt flag for watchdog timer
 	
-	SW2_debounce_counter_press = 0;
-	SW2_debounce_counter_release = 0;
+	SW1_debounce_counter_press = 0;
+	SW1_debounce_counter_release = 0;
+    SW2_debounce_counter_press = 0;
+    SW2_debounce_counter_release = 0;
 	for(;;)
 	{
 		_delay_ms(1);                                   // 1 ms delay
+		if(SW1_debounce_counter_press)
+        {
+            SW1_debounce_counter_press--;
+            if(!SW1_debounce_counter_press)
+                SW1_debounce_counter_release = 20;
+        }
+        else if(SW1_debounce_counter_release && !SW1)
+        {
+            SW1_debounce_counter_release--;
+            if(!SW1_debounce_counter_release)
+            {
+                P1IFG &= ~BIT0;                         // Clear interrupt flag for SW2
+                P1IE |= BIT0;                           // Enable interrupts for SW2
+            }
+        }
 		if(SW2_debounce_counter_press)
 		{
 			SW2_debounce_counter_press--;
@@ -60,10 +80,21 @@ int main(void)
 #pragma vector = PORT1_VECTOR
 __interrupt void Port1_ISR()
 {
-    P1IE &= ~BIT1;                  // Disable interrupts for SW2
-    P1IFG &= ~BIT1;                 // Clear interrupt flag for SW2
-    P2OUT ^= BIT1;                  // Toggle LED2
-    SW2_debounce_counter_press = 20;// Set SW2 debounce counter to 20 ms
+    if(SW1)
+    {
+        P1IE &= ~BIT0;                  // Disable interrupts for SW2
+        IFG1 &= ~WDTIFG;                // Clear WDT interrupt flag
+        IE1 ^= WDTIE;                   // Toggle interrupt enable for WDT
+        SW1_debounce_counter_press = 20;// Set SW1 debounce counter to 20 ms
+    }
+    if(SW2)
+    {
+        P1IE &= ~BIT1;                  // Disable interrupts for SW2
+        P2OUT ^= BIT1;                  // Toggle LED2
+        SW2_debounce_counter_press = 20;// Set SW2 debounce counter to 20 ms
+    }
+    P1IFG &= ~BIT0;                     // Clear interrupt flag for SW1
+    P1IFG &= ~BIT1;                     // Clear interrupt flag for SW2
 }
 
 #pragma vector = WDT_VECTOR
