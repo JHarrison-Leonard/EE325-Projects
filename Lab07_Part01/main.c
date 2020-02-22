214bf70fef25e074f2ab707b6d1a8633368518be
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-02-22 13:07:54 -0600

    Completed timer B setup and added WDTduty cycle logic

--- Lab07_Part01/main.c
+++ Lab07_Part01/main.c
@@ -16,10 +16,11 @@
 int main(void)
 {
 	// Peripheral setup
-	WDTCTL = WDT_XXX;			// WDT with XXX interval
-	TB1CCTL0 = OUTMOD_7;		//
-	TB1CTL = TBSSEL_1 + MC_1;	//
-	TB1CCR0
+	WDTCTL = WDT_ADLY_1_9;		// WDT with 1.9 ms ACLK interval
+	TB0CCTL1 = OUTMOD_7;		// Reset/Set output mode
+	TB0CTL = TBSSEL_1 | MC_1;	// ACLK clock source and Up mode
+	TBOCCR0 = 1579;				// 32768 / 1579 ~ 21 Hz
+	TB0CCR1 = 0;				// Initial duty cycle of 0%;
 	P1DIR &= ~(BIT0 | BIT1);	// Set SW1 and SW2 (P1.0,1) to input
 	P2DIR |= BIT2;				// Set LED1 (P2.2) to output
 	P2SEL |= BIT2;				// Set LED1 to special mode (TB1)
@@ -33,7 +34,7 @@ int main(void)
 	IFG1 &= WDTIFG;				// Clear interrupt flag for WDT
 	
 	// Enter sleep
-	_BIS_SR(LPM0_bits + GIE);
+	_BIS_SR(LPM3);
 	
 	// Default return if it exits sleep for some reason
 	return 0;
@@ -42,13 +43,25 @@ int main(void)
 #pragma vecotr = PORT1_VECTOR
 __interrupt void Port1_ISR()
 {
-	
+	P1IFG &= ~(BIT0 | BIT1);
 }
 
 #pragma vector = WDT_VECTOR
 __interrupt void WDT_ISR()
 {
-	
+	static unsigned char direction = 1; // 1 = increase, 0 = decrease
+	if(direction)
+	{
+		TB0CCR1++;				// 1.9 ms / 3 s * 1579 ~ 1
+		if(TB0CCR1 == 1579)		// After 3 s, duty cucle reaches 100%
+			direction = 0;		// Reverse duty cycle change direction
+	}
+	else
+	{
+		TB0CCR1--;				// Reverse of above
+		if(!TB0CCR1)
+			direction = 1;
+	}
 }
 
 
