a08747b7922a73cb0460d08a0af22ae3e48ccf07
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-02-22 14:07:17 -0600

    Added setup and WDT logic

--- Lab07_Part02/main.c
+++ Lab07_Part02/main.c
@@ -8,17 +8,44 @@
  * Date:        [Due...
  *---------------------------------------------------------------------------*/
 #include <msp430.h>
-#include <stdio.h>
-#include "main.h"
+
+// Timer max, 50% duty cycle, LED, ...
+unsigned int config = {0, 0, 0, 16, 8, BIT2, 33, 16, BIT1, 66, 33, (BIT1 | BIT2)};
+unsigned char configLength = 12;
 
 
 int main(void)
 {
-	// Stop watchdog timer
-	WDTCTL = WDTPW | WDTHOLD;
+	// Peripheral setup
+	WDTCTL = WDT_ADLY_1000;		// WDT: 1 s ACLK interval
+	TB0CTL = TBSSEL_1 | MC_1;	// Timer B: ACLK and Up mode
+	TB0CCTL4 = OUTMOD_7;		// TB4: Reset/set output mode
+	P2DIR |= (BIT1 | BIT2);		// LED1 and LED2 (P2.2,1): Output
+	P2OUT &= ~(BIT1 | BIT2);	// LED1 and LED2: Off
+	P3DIR |= BIT5;				// Buzzer (P3.5): Output
+	P3SEL |= BIT5;				// Buzzer: Special mode (TB4);
 	
-	// Main code
+	// Interrupt setup
+	_EINT();					// Enable global interrupts
+	IE1 |= WDTIE;				// Enable WDT interrupts
+	IFG1 &= ~WDTIFG;			// Clear interrupt flag for WDT
+	
+	// Enter sleep
+	_BIS_SR(LPM3);
 	
 	// Default return
 	return 0;
+}
+
+
+#pragma vector = WDT_VECTOR
+__interrupt void WDT_ISR()
+{
+	static unsigned char configSelect = 0;
+	TB0CCR0 = config[configSelect++];
+	TB0CCR4 = config[configSelect++];
+	P2OUT &= ~(BIT1 | BIT2);
+	P2OUT |= config[configSelect++];
+	if(configSelect >= configLength)
+		configSelect = 0;
 }
