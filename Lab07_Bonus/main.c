dbca916efae04458f9ea6231e4d72cb094ceeb91
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-02-25 16:55:11 -0600

    It works???

--- Lab07_Bonus/main.c
+++ Lab07_Bonus/main.c
@@ -18,7 +18,9 @@
 
 // blink_mode == 0, gradual change; blink_mode = 1, instant blink
 unsigned char blink_mode = 0;
+unsigned int blink_timer = 0;
 unsigned char timer_max = 188;
+unsigned char debounce = 0;
 
 
 int main(void)
@@ -31,6 +33,7 @@ int main(void)
 	TB0CCR1 = 0;				// Initial duty cycle of 0%;
 	P1DIR &= ~(BIT0 | BIT1);	// Set SW1 and SW2 (P1.0,1) to input
 	P2DIR |= BIT2;				// Set LED1 (P2.2) to output
+	P2OUT &= ~BIT2;
 	P2SEL |= BIT2;				// Set LED1 to special mode (TB1)
 	
 	// Interrupt setup
@@ -41,9 +44,11 @@ int main(void)
 	
 	for(;;)
 	{
-		LPM3;					// Enter deep (ACLK only) sleep
-		_delay_cycles(20000);	// 20 ms debounce delay
-		P1IE |= (BIT0 | BIT1);	//Enable interrupts for SW1 and SW2
+	    if(debounce)
+	    {
+	        _delay_cycles(2000);	// 20 ms debounce delay
+	        P1IE |= (BIT0 | BIT1);	//Enable interrupts for SW1 and SW2
+	    }
 	}
 }
 
@@ -85,8 +90,8 @@ __interrupt void Port1_ISR()
 			IE1 |= WDTIE;		// Resuming duty cycle changes
 			if(SW1)
 			{
-				P2SEL &= ~BIT2;	// Disable timer control of LED2
 				blink_mode = 1;	// Instant blink
+				blink_timer = TB0CCR1;
 				timer_max = 8;  // 4 Hz blink
 			}
 		}
@@ -100,7 +105,7 @@ __interrupt void Port1_ISR()
 		}
 	}
 	
-	LPM3_EXIT;					// Exit sleep for debounce
+	debounce = 1;
 	P1IFG &= ~(BIT0 | BIT1);	// Clear interrupt flag for SW1 and SW2
 }
 
@@ -109,26 +114,42 @@ __interrupt void WDT_ISR()
 {
 	static unsigned char direction = 1;	// 1 = increase, 0 = decrease
 	
-	if(direction)
-	{								// 16 ms / 3 s * 188 ~ 1
-		TB0CCR1++;					// Or 1 / 16 ms / 8 Hz ~ 8
-		if(TB0CCR1 == timer_max)	// After duty cycle reaches 100%
-		{
-			direction = 0;			// Reverse duty cycle change direction
-			if(blink_mode)			// If timer mode is instant change
-				P2OUT |= BIT2;		// Turn LED1 on
-		}
-	}
-	else
-	{
-		TB0CCR1--;					// Reverse of above
-		if(!TB0CCR1)
-		{
-			direction = 1;
-			if(blink_mode)
-				P2OUT &= ~BIT2;
-		}
-	}
+    if(direction)
+    {
+        if(!blink_mode)
+        {                                // 16 ms / 3 s * 188 ~ 1
+            TB0CCR1++;					// Or 1 / 16 ms / 8 Hz ~ 8
+            if(TB0CCR1 >= timer_max)	// After duty cycle reaches 100%
+                direction = 0;			// Reverse duty cycle change direction
+        }
+        else
+        {
+            blink_timer++;
+            if(blink_timer >= timer_max)
+            {
+                direction = 0;
+                P2SEL |= BIT2;
+            }
+        }
+    }
+    else
+    {
+        if(!blink_mode)
+        {
+            TB0CCR1--;					// Reverse of above
+            if(!TB0CCR1)
+                direction = 1;
+        }
+        else
+        {
+            blink_timer--;
+            if(!blink_timer)
+            {
+                direction = 1;
+                P2SEL &= ~BIT2;
+            }
+        }
+    }
 }
 
 
