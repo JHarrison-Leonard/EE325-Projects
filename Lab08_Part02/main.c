90edf22969fc2a8f92d30105b888b7132d0b5283
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-03-02 12:39:20 -0600

    Added echo UART_get and first half of logic

--- Lab08_Part02/main.c
+++ Lab08_Part02/main.c
@@ -9,6 +9,7 @@
  *---------------------------------------------------------------------------*/
 #include <msp430.h>
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 #include "serialIO.h"
 
@@ -18,19 +19,47 @@ int main(void)
 	// Stop watchdog timer
 	WDTCTL = WDTPW | WDTHOLD;
 	
+	// Peripherals setup
+	UART_initialize();
+	
+	
+	char buffer[80];
+	
 	for(;;)
 	{
 		do
 		{
-			// "Hi, I am CalcBot!" Logic
-		}while(/* "Help me..." */);
-		// "Would you like to..."
-		while(/* Invalid operation */)
+			UART_sendString("Hi, I am CalcBot! Do you need help?\n");
+			UART_getLine_echo(buffer, sizeof(buffer));
+		}while(!strcmp(buffer, "Help me, CalcBot!"));
+		UART_sendString("Would you like to 'add', 'subtract' or 'multiply'?\n");
+		UART_getLine_echo(buffer, sizeof(buffer));
+		while(!(strcmp(buffer, "add") || strcmp(buffer, "subtract") || strcmp(buffer, "multiply")))
 		{
-			// "...is not a valid operation..."
+			UART_sendString(buffer);
+			UART_sendString("is not a valid operation. Try again!");
+			UART_getLine_echo(buffer, sizeof(buffer));
 		}
 		// Get first operand
 		// Get second operand
 		// Calc and out
 	}
+}
+
+char UART_getCharacter_echo()
+{
+	char c;							// Buffer variable
+	while(!(IFG2 & UCA0RXIFG));		// Wait for character recieve
+	c = UCA0RXBUF;					// Store recieved character to c
+	UART_sendCharacter(c);			// Echo recieved character
+	return c;						// Return recieved character
+}
+
+void UART_getLine_echo(char * buf, int limit)
+{
+	int i = 0;
+	char c;							// Buffer variable
+	while((i < limit - 1) && ((c = UART_getCharacter_echo()) != '\n'))
+		buf[i++] = c;				// Get characters until buffer limit or newline
+	c[i] = '\0';					// Terminate with a null character
 }
--- Lab08_Part02/main.h
+++ Lab08_Part02/main.h
@@ -9,10 +9,18 @@
  *---------------------------------------------------------------------------*/
 #ifndef MAIN_H_
 #define MAIN_H_
-#include "serialIO.h"
 
 
 int main(void);
 
+/* Same functionality as UART_getCharacter with the added change of calling
+ * UART_sendCharacter for the recieved character
+ */
+char UART_getCharacter_echo();
+
+/* Copy of UART_getLine from serialIO.h, with the change of calling
+ * UART_getCharacter_echo instead of UART_getCharacter
+ */
+void UART_getLine_echo(char * buf, int limit);
 
 #endif
