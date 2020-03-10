ddb96f9eae73fb013adb91f23fa64a1161b570ab
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-03-10 10:29:10 -0500

    Added some setup skeleton

--- Lab09_Part02/main.c
+++ Lab09_Part02/main.c
@@ -8,17 +8,22 @@
  * Date:        [Due...
  *---------------------------------------------------------------------------*/
 #include <msp430.h>
-#include <stdio.h>
-#include "main.h"
 
 
 int main(void)
 {
-	// Stop watchdog timer
-	WDTCTL = WDTPW | WDTHOLD;
-	
-	// Main code
+	// Peripheral setup
+	WDTCTL = 0x00;			// Some interval mode non aclk timing
+	SPI_initialize();
 	
 	// Default return
 	return 0;
+}
+
+void SPI_initialize()
+{
+	USICTL0 |= USISWRST;	// Software reset
+	USICTL0 |= USIPE5 | USIPE6 | USIPE7 | USIOE;
+	USICTL1 = USIIE;		// USI Interrupt enable
+	USICTL0 &= ~USISWRST;	// Exit software reset
 }
