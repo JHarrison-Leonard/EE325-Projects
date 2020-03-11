91106586b3910ac67a717bdc6480d9233ca19fb6
JHLeonard <JHarrison.Leonard@yahoo.com>
2020-03-11 08:20:43 -0500

    Cleaned up and made more robust

--- Lab09_Part01/serialIO.c
+++ Lab09_Part01/serialIO.c
@@ -5,7 +5,7 @@
  * Output:      UART0
  * Author:      Justin H. Leonard
  * Lab Section: 04
- * Date:        March 5th, 2020
+ * Serial:      2020031100
  *---------------------------------------------------------------------------*/
 #include <msp430.h>
 #include "serialIO.h"
@@ -16,15 +16,16 @@ void UART_initialize()
 	UCA0CTL1 |= UCSWRST;		// Set software reset during initialization
 	P2SEL |= BIT4 | BIT5;		// UCA0 TX and RX mode (P2.4,5)
 	UCA0CTL1 |= UCSSEL_2;		// BRCLK = SMCLK = 1048576 Hz
-	UCA0BR0 = 0x09;				// Baud rate = 115200
-	UCA0BR1 = 0x00;				// BRCLK / baud rate = 1048576 / 19200 = 9
+	UCA0BR0 = (char) 1048576 / SIO_UART_BR;
+	UCA0BR1 = (char) 1048576 / (256 * SIO_UART_BR);
 	UCA0CTL1 &= ~UCSWRST;		// Initialized, unset software reset
 }
 
 void UART_sendCharacter(char c)
 {
-	if(c == '\r')				// Carriage return before newlines
-		UART_sendCharacter('\n');
+	if(c == '\n')				// Carriage returns before newlines
+		UART_sendCharacter('\r');
+	
 	while(!(IFG2 & UCA0TXIFG));	// Wait until TXBUF is empty
 	UCA0TXBUF = c;				// Send c
 }
@@ -52,19 +53,20 @@ void UART_getLine(char * buf, int limit)
 
 void UART_getLine_echo(char * buf, int limit)
 {
-    int i = 0;
-    char c;                         // Buffer variable
-    while((i < limit - 1) && ((c = UART_getCharacter()) != '\r'))
-    {
-        if((i > 0) || (c != '\x7f'))
-        {
-            UART_sendCharacter(c);
-            if(c == '\x7f')
-                i--;
-            else
-                buf[i++] = c;       // Get characters until buffer limit or newline
-        }
-    }
-    UART_sendCharacter(c);
-    buf[i] = '\0';                  // Terminate with a null character
+	int i = 0;
+	char c;						// Buffer variable
+	while((i < limit - 1) && ((c = UART_getCharacter()) != SIO_UART_NEWLINE))
+	{
+		if((i > 0) || (c != SIO_UART_BACKSPACE))
+		{
+			UART_sendCharacter(c);
+			if(c == SIO_UART_BACKSPACE)
+				i--;
+			else
+				buf[i++] = c;	// Get characters until buffer limit or newline
+		}
+	}
+	if(i < limit - 1)
+		UART_sendCharacter('\n');
+	buf[i] = '\0';				// Terminate with a null character
 }
--- Lab09_Part01/serialIO.h
+++ Lab09_Part01/serialIO.h
@@ -5,12 +5,28 @@
  * Output:      N/A
  * Author:      Justin H. Leonard
  * Lab Section: 04
- * Date:        March 5th, 2020
+ * Serial:      2020031100
  *---------------------------------------------------------------------------*/
 #ifndef SERIALIO_H_
 #define SERIALIO_H_
 
-/* "Configures UCI to work in UART mode at the baud rate as follows: if your
+// Default baud rate
+#ifndef SIO_UART_BR
+#define SIO_UART_BR 9600
+#endif
+
+// Default input backspace character
+#ifndef SIO_UART_BACKSPACE
+#define SIO_UART_BACKSPACE '\x7f'
+#endif
+
+// Default input newline character
+#ifndef SIO_UART_NEWLINE
+#define SIO_UART_NEWLINE '\r'
+#endif
+
+
+/*   "Configures UCI to work in UART mode at the baud rate as follows: if your
  * first name has an even number of letters use 19200, otherwise use 115200."
  * My first name is "Justin" so the baud rate is set to 19200
  *
@@ -35,14 +51,14 @@ void UART_sendCharacter(char c);
  */
 char UART_getCharacter();
 
-/* "Sends a string via UART using UART_sendCharacter(char c)"
+/*   "Sends a string via UART using UART_sendCharacter(char c)"
  * Does not send the null character.
  *
  * Inputs: str: The string to be sent via UART
  */
 void UART_sendString(char * str);
 
-/* "Receives characters via UART using UART_getCharacter() until it finds the
+/*   "Receives characters via UART using UART_getCharacter() until it finds the
  * new line character or until the limit of characters is exceeded. Write that
  * string (excluding the new line character) to the buffer allocated outside of
  * the function. Terminates the string with the null character."
@@ -54,6 +70,15 @@ void UART_sendString(char * str);
  */
 void UART_getLine(char * buf, int limit);
 
+/*   Same functionality as UART_getLine with added functionality of the
+ * SIO_UART_BASKSPACE backspace character working as expected as well as echoing
+ * recieved inputs to the output.
+ *
+ * Inputs:   buf: The location in memory to store the recieved characters
+ *           limit: The limit to recieve which is to be at most equal to the
+ *                  size of buf
+ * Modifies: buf
+ */
 void UART_getLine_echo(char * buf, int limit);
 
 #endif
